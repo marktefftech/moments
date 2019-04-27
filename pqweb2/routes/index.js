211a52954ecbd75bc9131a873b7e9a4307049e03
Mark <markteffeteller@Marks-MBP.lan1>
2019-04-27 12:44:15 -0500

    add express and mongodb

--- pqweb2/routes/index.js
+++ pqweb2/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'c.z.Moments' });
 });
+
+/* GET Userlist page.*/
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
 
 module.exports = router;
